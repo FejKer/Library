31bb17cd752c591c49973c3a4402d2962f44b362
BuildTools <unconfigured@null.spigotmc.org>
2022-07-06 02:47:26 +0200

    Added function to borrow and return resources

--- Biblioteka/Program.cs
+++ Biblioteka/Program.cs
@@ -13,8 +13,7 @@ namespace Biblioteka
             public int id { get; private set; }         //id konkretnego egzemplarza
             public string group_id { get; set; }           //id egzemplarza - kod kreskowy
             public string name { get; set; }            //nazwa egzemplarza
-            public int quantity { get; set; }           //ilosc egzemplarzy
-            public bool isAvailable { get; set; }       //czy przedmiot mozna wypozyczyc?
+            public bool isBorrowed { get; set; }       //czy przedmiot mozna wypozyczyc?
 
             public Zasoby()
             {
@@ -72,9 +71,11 @@ namespace Biblioteka
         {
             private static int last_customer_id;
             public int id { get; private set; }
+            public string name { get; private set; }
             public List<Zasoby> wypozyczone;
-            public Customer()
+            public Customer(string name)
             {
+                this.name = name;
                 id = Interlocked.Increment(ref last_customer_id);                      //id klienta z automatyczną inkrementacją
                 this.wypozyczone = new List<Zasoby>();                                 //wypozyczone zasoby
             }
@@ -85,13 +86,12 @@ namespace Biblioteka
             var program = new Program();
             var zasoby = new List<Zasoby>();                                            //zmienna przechowująca wszystkie zasoby
             var customers = new List<Customer>();                                       //zmienna przechowująca wszystkich klientów
-            Dictionary<string, int> quantities = new Dictionary<string, int>();
+            Dictionary<string, int> quantities = new Dictionary<string, int>();         //zmienna przechowująca ilości poszczególnych egzemplarzy
             
-            program.printMainMenu(zasoby, quantities);
-
+            program.printMainMenu(zasoby, quantities, customers);
         }
 
-        void printMainMenu(dynamic zasoby, Dictionary<string, int> quantities)
+        void printMainMenu(dynamic zasoby, Dictionary<string, int> quantities, dynamic customers)
         {
             Console.Clear();
             Console.WriteLine("===================Biblioteka===================");
@@ -102,16 +102,144 @@ namespace Biblioteka
             Console.WriteLine("5.Wyświetl zasoby.");
             Console.WriteLine("6.Zapisz stan do pliku.");
             Console.WriteLine("7.Wczytaj stan z pliku.");
+            Console.WriteLine();
             int n = Convert.ToInt32(Console.ReadLine());                                   //czytamy wybor uzytkownika
 
             switch (n)
             {
-                case 1:
-                case 2: addResource(zasoby, quantities); printMainMenu(zasoby, quantities); break;
-                case 3: removeResource(zasoby, quantities); printMainMenu(zasoby, quantities); break;
-                case 4:
-                case 5: printResources(zasoby, quantities); printMainMenu(zasoby, quantities); break;
-                default: Console.WriteLine("Niewłaściwy wybór."); printMainMenu(zasoby, quantities); break;
+                case 1: rentResource(customers, zasoby); printMainMenu(zasoby, quantities, customers); break;
+                case 2: addResource(zasoby, quantities); printMainMenu(zasoby, quantities, customers); break;
+                case 3: removeResource(zasoby, quantities); printMainMenu(zasoby, quantities, customers); break;
+                case 4: rentResource(customers, zasoby); printMainMenu(zasoby, quantities, customers); break;
+                case 5: printResources(zasoby, quantities); printMainMenu(zasoby, quantities, customers); break;
+                default: Console.WriteLine("Niewłaściwy wybór."); printMainMenu(zasoby, quantities, customers); break;
+            }
+        }
+        void rentResource(dynamic customers, dynamic zasoby)
+        {
+            Console.WriteLine("===================WYPOŻYCZENIE/ZWROT ZASOBU===================");
+            Console.WriteLine("1. Nowy klient.");
+            Console.WriteLine("2. Powracający klient.");
+            int n = Convert.ToInt32(Console.ReadLine());
+            switch (n)
+            {
+                case 1: AddCustomer(customers, zasoby); break;
+                case 2: login(customers, zasoby); break;
+                default: Console.WriteLine("Nieprawidłowy wybór."); rentResource(customers, zasoby); break;
+            }
+        }
+        void AddCustomer(dynamic customers, dynamic zasoby)
+        {
+            Console.WriteLine("Wpisz login, którego chcesz używać:");
+            string name = Console.ReadLine();
+
+            foreach(Customer customer in customers)
+            {
+                if(customer.name == name)
+                {
+                    Console.WriteLine("Login jest już zajęty.");
+                    Console.WriteLine("Naciśnij dowolny przycisk.");
+                    Console.ReadKey();
+                    AddCustomer(customers, zasoby);
+                }
+            }
+
+            customers.Add(new Customer(name));
+            Console.WriteLine("\nRejestracja pomyślna.");
+            updateResource(zasoby, customers, name);
+        }
+        void login(dynamic customers, dynamic zasoby)
+        {
+            bool logged = false;
+            Console.WriteLine("Podaj login.");
+            string name = Console.ReadLine();
+            foreach (Customer customer in customers)
+            {
+                if(customer.name == name)
+                {
+                    Console.WriteLine("Zalogowano");
+                    logged = true;
+                }
+            }
+            if (logged)
+            {
+                Console.WriteLine("1. Wypożyczenie.");
+                Console.WriteLine("2. Zwrot.");
+                int n = Convert.ToInt32(Console.ReadLine());
+                if(n == 1)
+                {
+                    updateResource(zasoby, customers, name);
+                } else if(n == 2)
+                {
+                    returnResource(zasoby, customers, name);
+                }
+                else
+                {
+                    Console.WriteLine("Niepoprawny wybór.");
+                    login(customers, zasoby);
+                }
+            } 
+            else
+            {
+                Console.WriteLine("Niepoprawny login.");
+                Console.WriteLine("Naciśnij dowolny przycisk.");
+                Console.ReadKey();
+                login(customers, zasoby);
+            }
+        }
+        void updateResource(dynamic zasoby, dynamic customers, string name)
+        {
+            Customer customer = null;
+            Console.WriteLine("Podaj ID zasobu, który chcesz wypożyczyć.");
+            int n = Convert.ToInt32(Console.ReadLine());
+            foreach(Customer c in customers)
+            {
+                if(c.name == name)
+                {
+                    customer = c;
+                }
+            }
+            foreach(Zasoby z in zasoby)
+            {
+                if (z.id == n && z.isBorrowed == false)
+                {
+                    z.isBorrowed = true;
+                    customer.wypozyczone.Add(z);
+                    Console.WriteLine("Wypożyczono zasób.");
+                    Console.WriteLine("Naciśnij dowolny przycisk.");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Niepoprawny numer ID lub zasób jest już wypożyczony.");
+                    updateResource(zasoby, customers, name);
+                }
+            }
+        }
+        void returnResource(dynamic zasoby, dynamic customers, string name)
+        {
+            Customer customer = null;
+            foreach (Customer c in customers)
+            {
+                if (c.name == name)
+                {
+                    customer = c;
+                }
+            }
+            foreach (Zasoby z in customer.wypozyczone)
+            {
+                Console.WriteLine("ID: " + z.id);
+                Console.WriteLine("Nazwa: " + z.name);
+            }
+            Console.WriteLine("Podaj ID zasobu, który chcesz zwrócić.");
+            int n = Convert.ToInt32(Console.ReadLine());
+            foreach(Zasoby z in zasoby)
+            {
+                if(z.id == n)
+                {
+                    z.isBorrowed = false;
+                    customer.wypozyczone.Remove(z);
+                }
             }
         }
         void removeResource(dynamic zasoby, Dictionary<string, int> quantities)
@@ -138,6 +266,11 @@ namespace Biblioteka
             {
                 quantityHandlerRemove(quantities, group_id);
             }
+            else
+            {
+                Console.WriteLine("Błąd podczas usuwania.\n Naciśnij dowolny klawisz.");
+                Console.ReadKey();
+            }
         }
         void printResources(dynamic zasoby, Dictionary<string, int> quantities)
         {
@@ -147,9 +280,11 @@ namespace Biblioteka
                 int q;
                 quantities.TryGetValue(z.group_id, out q);
                 Console.WriteLine("===================================");
-                Console.WriteLine("ID: " + z.id + "\nKod Kreskowy: " + z.group_id + "\nNazwa: " + z.name + "\nCzy wypożyczone? " + z.isAvailable + "\nIlość egzemplarzy: " + q);
+                Console.WriteLine("ID: " + z.id + "\nKod Kreskowy: " + z.group_id + "\nNazwa: " + z.name + "\nCzy wypożyczone? " + z.isBorrowed + "\nIlość egzemplarzy: " + q);
                 Console.WriteLine("===================================");
             }
+            Console.WriteLine();
+            Console.WriteLine("Naciśnij dowolny klawisz.");
             Console.ReadKey();
         }
         void addResource(dynamic zasoby, Dictionary<string, int> quantities)
@@ -227,11 +362,11 @@ namespace Biblioteka
         {
             try
             {
-                quantities.Add(group_id, 1);
+                quantities.Add(group_id, 1);                    // jeśli w zbiorze nie ma danego kodu kreskowego, dodajemy go i ustawiamy ilość egzemplarzy na 1
             }
             catch (Exception ex)
             {
-                int temp = quantities[group_id] + 1;
+                int temp = quantities[group_id] + 1;            // jeśli w zbiorze już istnieje dany kod, zwiększamy zmienną ilości egzemplarzy
                 quantities[group_id] = temp;
             }
         }
